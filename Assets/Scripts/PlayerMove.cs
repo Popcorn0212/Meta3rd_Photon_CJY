90a08e6a25cd2f4a58dd4673fb0ac55c07f6b3b5
Popcorn0212 <cwd8564@gmail.com>
2024-08-26 11:49:02 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 이동속도
+    float speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mx = Input.GetAxisRaw("Horizontal");
+        float mz = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(mx, 0, mz);
+        dir.Normalize();
+
+        dir = transform.TransformDirection(dir);
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+
+// 오른쪽으로 정확하게 5만큼 이동하고 싶다.
+public class TestMove : MonoBehaviour
+{
+    // 이동거리
+    float moveDist = 0;
+    // 움직일 수 있는지
+    bool isMove = true;
+
+    void Start()
+    {
+        
+        
+    }
+
+    void Update()
+    {
+        if (isMove)
+        {
+            // 1. 오른쪽으로 움직이기
+            transform.position += Vector3.right * 5 * Time.deltaTime;
+            // 이동거리 누적
+            moveDist += 5 * Time.deltaTime;
+            // 2. 만약에 움직인 거리가 5 보다 커지면
+            if (moveDist >= 5)
+            {
+                // 3. 멈추기
+                isMove = false;
+                // 4. 오버 된 이동거리 만큼 왼쪽으로 이동시키자
+                float overDist = moveDist - 5;
+                transform.position += Vector3.left * overDist;
+            }
+        }
+    }
+}
